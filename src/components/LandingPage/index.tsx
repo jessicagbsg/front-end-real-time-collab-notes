44de602de32c50eb3c781b1b8613f797a9aebf64
Jessica Gondim <jessicagbsg@gmail.com>
2024-08-21 22:02:42 -0300

    chore: init

---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../ui/button";
+
+export function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the landing page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
